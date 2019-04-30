bf572f6bca98fe7dad4206d6218d5f0947ae09da
Victor Cornillere <v.cornillere@gmail.com>
2019-04-30 11:43:36 +0200

    Add time scaling when pressing + and -

--- src/systems/digestion.rs
+++ src/systems/digestion.rs
@@ -1,5 +1,5 @@
-use amethyst::{core::Transform, core::Time, ecs::*};
 use amethyst::renderer::*;
+use amethyst::{core::Time, core::Transform, ecs::*};
 use std::f32;
 
 use crate::components::digestion::{Digestion, Fullness};
@@ -14,10 +14,11 @@ impl<'s> System<'s> for DigestionSystem {
     );
 
     fn run(&mut self, (digestions, mut fullnesses, time): Self::SystemData) {
+        let delta_time = time.delta_seconds();
         for (digestion, fullness) in (&digestions, &mut fullnesses).join() {
-            let burned = digestion.nutrition_burn_rate * time.fixed_seconds();
+            let burned = digestion.nutrition_burn_rate * delta_time;
             let new_value = fullness.value - burned;
-            fullness.value = if burned < f32::EPSILON { 0.0 } else { new_value };
+            fullness.value = new_value;
         }
     }
 }
@@ -26,10 +27,7 @@ pub struct StarvationSystem;
 
 // Entities die if their fullness reaches zero (or less).
 impl<'s> System<'s> for StarvationSystem {
-    type SystemData = (
-        ReadStorage<'s, Fullness>,
-        Entities<'s>,
-    );
+    type SystemData = (ReadStorage<'s, Fullness>, Entities<'s>);
 
     fn run(&mut self, (fullnesses, entities): Self::SystemData) {
         for (fullness, entity) in (&fullnesses, &*entities).join() {
