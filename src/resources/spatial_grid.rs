657f0796fac7bee7dc13937ce5176552bd092aea
Victor <v.cornillere@gmail.com>
2019-05-17 08:08:59 +0200

    Fix spatial grid query not covering all the cells it should

--- src/resources/spatial_grid.rs
+++ src/resources/spatial_grid.rs
@@ -37,14 +37,15 @@ impl SpatialGrid {
         let x_cell = (pos[0] / self.cell_size).floor() as i32;
         let y_cell = (pos[1] / self.cell_size).floor() as i32;
 
-        let integer_range = (range / self.cell_size).ceil() as i32;
+        let integer_range = 1 + (range / self.cell_size).ceil() as i32;
+        //        let sq_range = range * range;
 
         let mut entities = Vec::new();
         for x in -integer_range..integer_range {
             for y in -integer_range..integer_range {
-                if (x * x + y * y) as f32 > range * range {
-                    continue;
-                }
+                //                if (x * x + y * y) as f32 > sq_range {
+                //                    continue;
+                //                }
                 match self.cells.get(&(x_cell + x)) {
                     Some(col) => match col.get(&(y_cell + y)) {
                         Some(cell) => entities.extend_from_slice(cell.as_slice()),
