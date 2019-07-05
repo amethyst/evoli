a31e35c18a80ecbb79b68b86d3f17d4d28bc6295
Victor Cornillere <v.cornillere@gmail.com>
2019-07-05 15:42:50 +0200

    Fix time control UI

--- src/systems/time_control.rs
+++ src/systems/time_control.rs
@@ -19,17 +19,17 @@ pub fn create_time_control_ui(world: &mut World) {
         )
     };
 
-    //let _ = UiButtonBuilder::<String, String>::new("Pause")
-    //.with_id("pause button".to_string())
-    //.with_anchor(Anchor::BottomRight)
-    //.with_size(80.0, 36.0)
-    //.with_position(-255.0, 20.0)
-    //.with_font(font_handle.clone())
-    //.with_font_size(24.0f32)
-    //.with_text_color([0.0f32, 0.0, 0.0, 1.0])
-    //.with_hover_text_color([0.2f32, 0.2f32, 0.2f32, 1.0f32])
-    //.with_press_text_color([0.5, 0.5, 0.5, 1.0])
-    //.build_from_world(world);
+    let _ = UiButtonBuilder::<(), u32>::new("Pause")
+        .with_id(0u32)
+        .with_anchor(Anchor::BottomRight)
+        .with_size(80.0, 36.0)
+        .with_position(-255.0, 20.0)
+        .with_font(font_handle.clone())
+        .with_font_size(24.0f32)
+        .with_text_color([0.0f32, 0.0, 0.0, 1.0])
+        .with_hover_text_color([0.2f32, 0.2f32, 0.2f32, 1.0f32])
+        .with_press_text_color([0.5, 0.5, 0.5, 1.0])
+        .build_from_world(world);
 
     //let _ = UiButtonBuilder::<String, String>::new("Slow Down")
     //.with_id("slow down button".to_string())
@@ -60,6 +60,10 @@ pub fn create_time_control_ui(world: &mut World) {
 pub struct TimeControlSystem {
     ui_reader_id: Option<ReaderId<UiEvent>>,
     input_reader_id: Option<ReaderId<InputEvent<String>>>,
+
+    pause_button: Option<UiButton>,
+    speed_up_button: Option<UiButton>,
+    slow_down_button: Option<UiButton>,
 }
 
 impl<'s> System<'s> for TimeControlSystem {
@@ -77,47 +81,110 @@ impl<'s> System<'s> for TimeControlSystem {
             res.fetch_mut::<EventChannel<InputEvent<String>>>()
                 .register_reader(),
         );
+
+        let font_handle = {
+            let loader = res.fetch::<Loader>();
+            let font_storage = res.fetch::<AssetStorage<FontAsset>>();
+            loader.load(
+                "assets/fonts/OpenSans-Regular.ttf",
+                TtfFormat,
+                (),
+                &font_storage,
+            )
+        };
+        let button_resources = UiButtonBuilderResources::<(), u32>::fetch(&res);
+        self.pause_button = Some(
+            UiButtonBuilder::<(), u32>::new("Pause")
+                .with_id(0u32)
+                .with_anchor(Anchor::BottomRight)
+                .with_size(80.0, 36.0)
+                .with_position(-255.0, 20.0)
+                .with_font(font_handle.clone())
+                .with_font_size(24.0f32)
+                .with_text_color([0.0f32, 0.0, 0.0, 1.0])
+                .with_hover_text_color([0.2f32, 0.2f32, 0.2f32, 1.0f32])
+                .with_press_text_color([0.5, 0.5, 0.5, 1.0])
+                .build(button_resources)
+                .1,
+        );
+
+        let button_resources = UiButtonBuilderResources::<(), u32>::fetch(&res);
+        self.slow_down_button = Some(
+            UiButtonBuilder::<(), u32>::new("Slow Down")
+                .with_id(1u32)
+                .with_anchor(Anchor::BottomRight)
+                .with_size(100.0, 36.0)
+                .with_position(-160.0, 20.0)
+                .with_font(font_handle.clone())
+                .with_font_size(24.0f32)
+                .with_text_color([0.0f32, 0.0, 0.0, 1.0])
+                .with_hover_text_color([0.2f32, 0.2f32, 0.2f32, 1.0f32])
+                .with_press_text_color([0.5, 0.5, 0.5, 1.0])
+                .build(button_resources)
+                .1,
+        );
+
+        let button_resources = UiButtonBuilderResources::<(), u32>::fetch(&res);
+        self.speed_up_button = Some(
+            UiButtonBuilder::<(), u32>::new("Speed Up")
+                .with_id(2u32)
+                .with_anchor(Anchor::BottomRight)
+                .with_size(100.0, 36.0)
+                .with_position(-55.0, 20.0)
+                .with_font(font_handle.clone())
+                .with_font_size(24.0f32)
+                .with_text_color([0.0f32, 0.0, 0.0, 1.0])
+                .with_hover_text_color([0.2f32, 0.2f32, 0.2f32, 1.0f32])
+                .with_press_text_color([0.5, 0.5, 0.5, 1.0])
+                .build(button_resources)
+                .1,
+        );
     }
 
     fn run(&mut self, (finder, ui_events, mut ui_texts, mut input_events): Self::SystemData) {
         for event in ui_events.read(self.ui_reader_id.as_mut().unwrap()) {
-            let mut handle_button = |button_name: &str, action_name: &str| {
-                if let Some(button_entity) = finder.find(button_name) {
-                    if button_entity == event.target {
-                        match event.event_type {
-                            UiEventType::Click => {
-                                input_events.single_write(InputEvent::ActionPressed(
-                                    action_name.to_string(),
-                                ));
-                            }
-                            _ => (),
+            match event.event_type {
+                UiEventType::Click => {
+                    let action_to_send = {
+                        if event.target == self.pause_button.as_ref().unwrap().image_entity {
+                            "TogglePause"
+                        } else if event.target
+                            == self.speed_up_button.as_ref().unwrap().image_entity
+                        {
+                            "SpeedUp"
+                        } else if event.target
+                            == self.slow_down_button.as_ref().unwrap().image_entity
+                        {
+                            "SlowDown"
+                        } else {
+                            ""
                         }
+                    };
+                    if action_to_send != "" {
+                        input_events
+                            .single_write(InputEvent::ActionPressed(action_to_send.to_string()));
                     }
                 }
-            };
-            handle_button("pause button", "TogglePause");
-            handle_button("speed up button", "SpeedUp");
-            handle_button("slow down button", "SlowDown");
+                _ => (),
+            }
         }
 
         for event in input_events.read(self.input_reader_id.as_mut().unwrap()) {
             match event {
-                InputEvent::ActionPressed(action_name) => {
-                    if let Some(button_entity) = finder.find("pause button_btn_txt") {
-                        if let Some(text) = ui_texts.get_mut(button_entity) {
-                            match action_name.as_ref() {
-                                "TogglePause" => {
-                                    if text.text == "Pause" {
-                                        text.text = "Play".to_string();
-                                    } else if text.text == "Play" {
-                                        text.text = "Pause".to_string();
-                                    }
-                                }
-                                _ => (),
+                InputEvent::ActionPressed(action_name) => match action_name.as_ref() {
+                    "TogglePause" => {
+                        let pause_button_text_entity =
+                            self.pause_button.as_ref().unwrap().text_entity;
+                        if let Some(text) = ui_texts.get_mut(pause_button_text_entity) {
+                            if text.text == "Pause" {
+                                text.text = "Play".to_string();
+                            } else if text.text == "Play" {
+                                text.text = "Pause".to_string();
                             }
                         }
                     }
-                }
+                    _ => (),
+                },
                 _ => (),
             }
         }
